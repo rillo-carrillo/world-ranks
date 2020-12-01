6c021fc1e13c07c135f59f5ecc5ec2193e08371c
Jose Angel Carrillo Rodriguez <jacr_caro@hotmail.com>
2020-11-30 20:56:09 -0600

    video ended

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import Layout from '../component/Layout/Layout'
+import styles from '../styles/Home.module.css'
+import SearchInput from '../component/SearchInput/SearchInput'
+import CountriesTable from '../component/CountriesTable/CountriesTable'
+import { useState } from 'react'
+export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("")
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.toLowerCase().includes(keyword) ||
+    country.region.toLowerCase().includes(keyword) ||
+    country.subregion.toLowerCase().includes(keyword));
+  const onInputChange = (e) => {
+    e.preventDefault()
+    setKeyword(e.target.value.toLowerCase())
+  }
+  return (
+    <Layout>
+      <div className={styles.inputContainer}>
+        <div className={styles.counts}>Found {countries.length} countries </div>
+        <div className={styles.input}>
+        <SearchInput placeholder="Filter by Name, Region or SubRegion" onChange={onInputChange} />
+        </div>
+        
+      </div>
+
+      <CountriesTable countries={filteredCountries} />
+    </Layout>
+  )
+}
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all');
+  const countries = await res.json()
+  return {
+    props: {
+      countries
+    }
+  }
+}
